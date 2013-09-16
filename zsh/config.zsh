9e79e4f0680208de698cc53d9e4d51437f21d7b8
Donovan C. Young <dyoung522@gmail.com>
2013-09-16 13:19:20 -0400

    customized config and prompt files

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -31,6 +31,9 @@ setopt HIST_REDUCE_BLANKS
 #   like: git comm-[tab]
 setopt complete_aliases
 
+zle -N newtab
+
+bindkey -v
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
