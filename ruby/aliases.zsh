f6af19603f7b8f90cad8e96871cc1ad6419ec4c9
Donovan C. Young <dyoung522@gmail.com>
2013-09-16 15:05:19 -0400

    Split system/aliases into separate topic/aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,3 +1,7 @@
 alias sc='script/console'
 alias sg='script/generate'
 alias sd='script/destroy'
+
+# Rails shortcuts
+alias migrate='rake db:migrate db:test:clone'
+alias railsnew="rails new . -m https://raw.github.com/RailsApps/rails-composer/master/composer.rb -T"
