467fd67b619489a1d415f5bdbb757a27622d1ee3
Donovan C. Young <dyoung522@gmail.com>
2013-10-24 12:44:18 -0400

    Modify homebrew install to cover additional packages

--- brew/install.sh
+++
@@ -1,8 +0,0 @@
-if test ! $(which brew) ; then
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" || exit
-fi
-
-for PKG in $(cat $(dirname $0)/packages) ; do
-  brew install $PKG
-done
-
