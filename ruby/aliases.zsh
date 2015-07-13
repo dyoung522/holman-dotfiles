f5e74f86aa674e903b12a01e9d868a347c4264c7
Donovan Young <dyoung522@gmail.com>
2015-07-13 18:14:16 -0400

    Oracle version of .dotfiles

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -5,3 +5,4 @@ alias sd='script/destroy'
 # Rails shortcuts
 alias migrate='rake db:migrate db:test:clone'
 alias railsnew="rails new . -m https://raw.github.com/RailsApps/rails-composer/master/composer.rb -T"
+alias bforeman='bundle exec foreman start -f Procfile.local'
