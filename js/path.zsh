4a1b30785bf53f4dbd6951c22c485b8961a77210
Donovan Young <dyoung522@gmail.com>
2015-09-25 12:40:00 -0700

    move pathmunge into a function

--- js/path.zsh
+++ js/path.zsh
@@ -1,2 +1,3 @@
+source ~/.dotfiles/functions/pathmunge
 export NVM_DIR="$HOME/.nvm"
 pathmunge "$NVM_DIR" # Add RVM to PATH for scripting
--- system/path.zsh
+++ system/path.zsh
@@ -1,13 +1,4 @@
-function pathmunge() {
-    if [ "$2" = "force" ] || ! echo $PATH | $(which egrep) -q "(^|:)$1($|:)" ; then
-        if [ "$2" = "after" ] ; then
-            PATH=$PATH:$1
-        else
-            PATH=$1:$PATH
-        fi
-        export PATH
-    fi
-}
+source ~/.dotfiles/functions/pathmunge
 
 # Modify the path
 pathmunge /usr/local/bin        force
