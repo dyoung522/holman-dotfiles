d233700a560dceb9e0377ecfb7b50bd162bcceeb
Donovan Young <dyoung522@gmail.com>
2015-10-06 14:34:39 -0400

    Switch node version on cd

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,3 +1,16 @@
 export NVM_DIR=~/.nvm
 source $(brew --prefix nvm)/nvm.sh
-nvm use $(cat ~/.nvmrc)
+
+[[ -f ~/.nvmrc ]] && export NODE_VERSION=$(cat ~/.nvmrc)
+
+function switch_node_version() {
+    if (( $+functions[nvm] )); then
+        if [[ -f '.nvmrc' ]]; then
+            nvm use > /dev/null
+        fi
+    fi
+}
+
+chpwd_functions=(${chpwd_functions[@]} "switch_node_version")
+
+switch_node_version
