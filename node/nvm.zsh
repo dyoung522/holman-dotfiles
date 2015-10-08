a4425ac5a5dcdb5b5980e3c6cf17495dd4aef341
Donovan C. Young <dyoung522@gmail.com>
2015-10-08 00:01:15 -0400

    remove nvm.symlink

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -4,11 +4,13 @@ source $(brew --prefix nvm)/nvm.sh
 [[ -f ~/.nvmrc ]] && export NODE_VERSION=$(cat ~/.nvmrc)
 
 function switch_node_version() {
-    if (( $+functions[nvm] )); then
-        if [[ -f '.nvmrc' ]]; then
-            nvm use > /dev/null
-        fi
+  if (( $+functions[nvm] )); then
+    if [[ -f '.nvmrc' ]]; then
+      nvm use $(cat .nvmrc) > /dev/null
+    else
+      nvm use $(cat ~/.nvmrc) > /dev/null
     fi
+  fi
 }
 
 chpwd_functions=(${chpwd_functions[@]} "switch_node_version")
